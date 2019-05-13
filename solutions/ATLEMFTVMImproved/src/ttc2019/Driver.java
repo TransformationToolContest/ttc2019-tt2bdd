2a0fdb3c91600915104f640a2b2021fdcb4b9478
Dennis Wagelaar <dwagelaar@gmail.com>
2019-05-13 09:26:12 +0200

    Split off ATLEMFTVMImproved solution.

---
+++ solutions/ATLEMFTVMImproved/src/ttc2019/Driver.java
@@ -0,0 +1,110 @@
+package ttc2019;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import ttc2019.metamodels.bdd.BDDPackage;
+import ttc2019.metamodels.tt.TTPackage;
+import ttc2019.metamodels.tt.TruthTable;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		try {
+			initialize();
+			load();
+			run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static ResourceSet repository;
+	private static String RunIndex;
+	private static String Model;
+	private static String Tool;
+
+	private static long stopwatch;
+
+	private static Solution solution;
+	private static String ModelPath;
+
+	private static Object loadFile(String path) {
+		Resource mRes;
+		try {
+			mRes = repository.getResource(URI.createFileURI(new File(path).getCanonicalPath()),	true);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return mRes.getContents().get(0);
+	}
+
+	static void load() throws IOException {
+		stopwatch = System.nanoTime();
+		solution.setTruthTable((TruthTable) loadFile(ModelPath));
+
+		URI uri = URI.createFileURI(new File("output.xmi").getCanonicalPath());
+		Resource outputResource = repository.createResource(uri);
+		outputResource.getContents().clear();
+		solution.setOutputResource(outputResource);
+
+		stopwatch = System.nanoTime() - stopwatch;
+		report(BenchmarkPhase.Load);
+	}
+
+	static void initialize() throws Exception {
+		stopwatch = System.nanoTime();
+
+		repository = new ResourceSetImpl();
+		repository.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+		repository.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+		repository.getPackageRegistry().put(TTPackage.eINSTANCE.getNsURI(), TTPackage.eINSTANCE);
+		repository.getPackageRegistry().put(BDDPackage.eINSTANCE.getNsURI(), BDDPackage.eINSTANCE);
+
+		Model = System.getenv("Model");
+		ModelPath = System.getenv("ModelPath");
+		RunIndex = System.getenv("RunIndex");
+		Tool = System.getenv("Tool");
+
+		solution = new Solution();
+
+		stopwatch = System.nanoTime() - stopwatch;
+		report(BenchmarkPhase.Initialization);
+	}
+
+	static void run() throws IOException {
+		stopwatch = System.nanoTime();
+		solution.run("TT2BDD");
+		stopwatch = System.nanoTime() - stopwatch;
+		report(BenchmarkPhase.Run);
+		solution.save();
+	}
+
+	static void report(BenchmarkPhase phase) {
+		System.out.println(String.format("%s;%s;%s;%s;Time;%s", Tool, Model, RunIndex, phase.toString(), Long.toString(stopwatch)));
+
+		if ("true".equals(System.getenv("NoGC"))) {
+			// nothing to do
+		} else {
+			Runtime.getRuntime().gc();
+			Runtime.getRuntime().gc();
+			Runtime.getRuntime().gc();
+			Runtime.getRuntime().gc();
+			Runtime.getRuntime().gc();
+		}
+
+		final long memoryUsed = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		System.out.println(String.format("%s;%s;%s;%s;Memory;%s", Tool, Model, RunIndex, phase.toString(), Long.toString(memoryUsed)));
+	}
+
+	enum BenchmarkPhase {
+		Initialization, Load, Run
+	}
+}
---
+++ solutions/ATLEMFTVMImproved/src/ttc2019/Solution.java
@@ -0,0 +1,76 @@
+package ttc2019;
+
+import java.io.IOException;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.m2m.atl.emftvm.EmftvmFactory;
+import org.eclipse.m2m.atl.emftvm.ExecEnv;
+import org.eclipse.m2m.atl.emftvm.Metamodel;
+import org.eclipse.m2m.atl.emftvm.Model;
+import org.eclipse.m2m.atl.emftvm.impl.resource.EMFTVMResourceFactoryImpl;
+import org.eclipse.m2m.atl.emftvm.util.ClassModuleResolver;
+import org.eclipse.m2m.atl.emftvm.util.ModuleResolver;
+import org.eclipse.m2m.atl.emftvm.util.TimingData;
+
+import ttc2019.metamodels.bdd.BDDPackage;
+import ttc2019.metamodels.tt.TTPackage;
+import ttc2019.metamodels.tt.TruthTable;
+
+public class Solution {
+
+	private TruthTable truthTable;
+	private Resource outputResource;
+
+	public TruthTable getTruthTable() {
+		return truthTable;
+	}
+
+	public void setTruthTable(TruthTable tt) {
+		this.truthTable = tt;
+	}
+
+	public void run(String moduleName) {
+		  ExecEnv env = EmftvmFactory.eINSTANCE.createExecEnv();
+          ResourceSet rs = new ResourceSetImpl();
+  
+          Metamodel ttMetamodel = EmftvmFactory.eINSTANCE.createMetamodel();
+          ttMetamodel.setResource(TTPackage.eINSTANCE.getCell().eResource());
+          env.registerMetaModel("TT", ttMetamodel);
+
+          Metamodel bddMetamodel = EmftvmFactory.eINSTANCE.createMetamodel();
+          bddMetamodel.setResource(BDDPackage.eINSTANCE.getTree().eResource());
+          env.registerMetaModel("BDD", bddMetamodel);
+
+          // loading models
+          rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put("emftvm", new EMFTVMResourceFactoryImpl());
+
+          Model inModel = EmftvmFactory.eINSTANCE.createModel();
+          inModel.setResource(truthTable.eResource());
+          env.registerInputModel("IN", inModel);
+
+          Model outModel = EmftvmFactory.eINSTANCE.createModel();
+          outModel.setResource(outputResource);
+          env.registerOutputModel("OUT", outModel);
+
+          ModuleResolver mr = new ClassModuleResolver(Solution.class);
+          TimingData td = new TimingData();
+          env.loadModule(mr, moduleName);
+          td.finishLoading();
+          env.run(td);
+          td.finish();
+	}
+
+	public void save() throws IOException {
+		outputResource.save(null);
+	}
+
+	public void setOutputResource(Resource outputResource) {
+		this.outputResource = outputResource;
+	}
+
+	public Resource getOutputResource() {
+		return outputResource;
+	}
+}
