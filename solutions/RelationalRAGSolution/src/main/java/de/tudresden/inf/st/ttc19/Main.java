26a1e9db729dd8de30e0f3e43d00bb2f13cdd730
Johannes Mey <johannes.mey@tu-dresden.de>
2019-06-04 15:54:10 +0200

    add new bdd transformation impl

--- solutions/RelationalRAGSolution/src/main/java/de/tudresden/inf/st/ttc19/Main.java
+++ solutions/RelationalRAGSolution/src/main/java/de/tudresden/inf/st/ttc19/Main.java
@@ -18,26 +18,43 @@ public class Main {
   public static void main(String[] args) {
     logger.info("Starting Relational RAG solution.");
 
-    TruthTableParser parser = new TruthTableParser();
-    TruthTable tt = parser.parse("test.ttmodel");
+    {
+      TruthTableParser parser = new TruthTableParser();
+      TruthTable tt = parser.parse("test.ttmodel");
 
-    StringBuilder builder = new StringBuilder();
-    tt.writeXMI(builder);
-    logger.info("Printed XMI:\n{}", builder.toString());
+      BDD simpleBDD = tt.simpleBDD();
 
-    BDD bdd = tt.simpleBDD();
-
-    StringBuilder bddBuilder = new StringBuilder();
-    try {
-      bdd.writeXMI(bddBuilder);
-      logger.info("BDD XMI:\n{}", bddBuilder.toString());
-      Path outputPath = Paths.get("relrag-test.bddmodel");
-      try (BufferedWriter writer = Files.newBufferedWriter(outputPath)) {
-        writer.write(bddBuilder.toString());
+      StringBuilder simpleBuilder = new StringBuilder();
+      try {
+        simpleBDD.writeXMI(simpleBuilder);
+        logger.info("BDD XMI:\n{}", simpleBuilder.toString());
+        Path outputPath = Paths.get("relrag-test-simple.bddmodel");
+        try (BufferedWriter writer = Files.newBufferedWriter(outputPath)) {
+          writer.write(simpleBuilder.toString());
+        }
+      } catch (Exception e) {
+        logger.error("Problems in writing simple bdd xmi");
+        logger.catching(e);
       }
-    } catch (Exception e) {
-      logger.error("Problems in writing bdd xmi");
-      logger.catching(e);
+    }
+    {
+      TruthTableParser parser = new TruthTableParser();
+      TruthTable tt = parser.parse("test.ttmodel");
+
+      BDD caseBdd = tt.caseBDD();
+
+      StringBuilder bddBuilder = new StringBuilder();
+      try {
+        caseBdd.writeXMI(bddBuilder);
+        logger.info("BDD XMI:\n{}", bddBuilder.toString());
+        Path outputPath = Paths.get("relrag-test-case.bddmodel");
+        try (BufferedWriter writer = Files.newBufferedWriter(outputPath)) {
+          writer.write(bddBuilder.toString());
+        }
+      } catch (Exception e) {
+        logger.error("Problems in writing case bdd xmi");
+        logger.catching(e);
+      }
     }
   }
 
