e67c8ef4b562203e674bcb7cb8ad860f45ae86af
Johannes Mey <johannes.mey@tu-dresden.de>
2019-06-11 15:00:15 +0200

    improve tests, fix indent bug

--- solutions/RelationalRAGSolution/src/test/java/de/tudresden/inf/st/ttc19/JastAddTest.java
+++ solutions/RelationalRAGSolution/src/test/java/de/tudresden/inf/st/ttc19/JastAddTest.java
@@ -7,24 +7,43 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JastAddTest {
 
   private static Logger logger = LogManager.getLogger(Test.class);
 
-  TruthTable loadTruthTable(String name) {
+  static Stream<String> truthTableFiles() {
+    return Stream.of(
+        "../../models/Test.ttmodel",
+        "../../models/GeneratedI4O2Seed42.ttmodel",
+        "../../models/GeneratedI8O2Seed68.ttmodel",
+        "../../models/GeneratedI8O4Seed68.ttmodel",
+        "../../models/GeneratedI10O2Seed68.ttmodel");
+  }
+
+  static Stream<String> generatedTruthTableFiles() {
+    return Stream.of(
+        "../../models/GeneratedI4O2Seed42.ttmodel",
+        "../../models/GeneratedI8O2Seed68.ttmodel",
+        "../../models/GeneratedI8O4Seed68.ttmodel",
+        "../../models/GeneratedI10O2Seed68.ttmodel");
+  }
+
+  private TruthTable loadTruthTable(String name) {
 
     TruthTable tt = null;
 
     TruthTableParser parser = new TruthTableParser();
 
-    try (InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(name)) {
+    try (InputStream stream = new FileInputStream(name)) {
       Assertions.assertNotNull(stream, "unable to load resource '" + name + "'");
       tt = parser.parse(stream);
     } catch (IOException e) {
@@ -36,13 +55,30 @@ public class JastAddTest {
     return tt;
   }
 
-  @Test
-  void testTruthTableRoundtrip() {
-    String file = "test.ttmodel";
+  private void validate(String tt, String bdd) throws IOException {
+    logger.debug("calling 'java -jar validator.jar {} {}'", tt, bdd);
+
+    Process proc = Runtime.getRuntime().exec(new String[]{"java", "-jar", "../../models/validator.jar", tt, bdd});
+    InputStream in = proc.getInputStream();
+    try {
+      proc.waitFor();
+    } catch (InterruptedException e) {
+      Assertions.fail(e);
+    }
+    byte b[] = new byte[in.available()];
+    in.read(b, 0, b.length);
+    String output = new String(b);
+    logger.debug("process returned {}", output);
+    Assertions.assertTrue(output.trim().endsWith("all OK"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("generatedTruthTableFiles")
+  void testTruthTableRoundtrip(String file) {
 
     String originalTT = null;
 
-    try (InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(file)) {
+    try (InputStream stream = new FileInputStream(file)) {
       originalTT = new BufferedReader(new InputStreamReader(stream)).lines().collect(Collectors.joining("\n"));
     } catch (IOException e) {
       Assertions.fail(e);
@@ -58,35 +94,43 @@ public class JastAddTest {
     Assertions.assertEquals(originalTT, roundtripTT);
   }
 
-  @Test
-  void testSimpleBDD() throws IOException {
-    TruthTable tt = loadTruthTable("test.ttmodel");
+  @ParameterizedTest
+  @MethodSource("truthTableFiles")
+  void testSimpleBDD(String pathName) throws IOException {
+    TruthTable tt = loadTruthTable(pathName);
+    File inputFile = new File(pathName);
 
     BDD simpleBDD = tt.simpleBDD();
 
     StringBuilder simpleBuilder = new StringBuilder();
     simpleBDD.writeXMI(simpleBuilder);
 
-    Path outputPath = Paths.get("relrag-test-simple.bddmodel");
+    Path outputPath = Files.createTempFile("relrag-test-simple", ".bddmodel");
+    outputPath.toFile().deleteOnExit();
     try (BufferedWriter writer = Files.newBufferedWriter(outputPath)) {
       writer.write(simpleBuilder.toString());
     }
 
+    validate(inputFile.getAbsolutePath(), outputPath.toString());
   }
 
-  @Test
-  void testCaseBDT() throws IOException {
-    TruthTable tt = loadTruthTable("test.ttmodel");
+  @ParameterizedTest
+  @MethodSource("truthTableFiles")
+  void testCaseBDT(String pathName) throws IOException {
+    TruthTable tt = loadTruthTable(pathName);
+    File inputFile = new File(pathName);
 
     BDD caseBdd = tt.caseBDD();
 
     StringBuilder bddBuilder = new StringBuilder();
     caseBdd.writeXMI(bddBuilder);
-    Path outputPath = Paths.get("relrag-test-case.bddmodel");
+    Path outputPath = Files.createTempFile("relrag-test-case", ".bddmodel");
+    outputPath.toFile().deleteOnExit();
     try (BufferedWriter writer = Files.newBufferedWriter(outputPath)) {
       writer.write(bddBuilder.toString());
     }
 
+    validate(inputFile.getAbsolutePath(), outputPath.toString());
   }
 
 }
