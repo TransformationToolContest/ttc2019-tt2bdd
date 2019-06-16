b6ca20cf2f70d85c571467a7fee7d6a50067d8a5
Antonio García-Domínguez <nyoescape@gmail.com>
2019-06-16 16:21:13 +0100

    validator: try all variations of each incomplete input truth table row

--- models/ttc2019.model.validator/src/ttc2019/model/validator/TreeValidator.java
+++ models/ttc2019.model.validator/src/ttc2019/model/validator/TreeValidator.java
@@ -1,7 +1,12 @@
 package ttc2019.model.validator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 import ttc2019.metamodels.bdd.Assignment;
 import ttc2019.metamodels.bdd.BDD;
@@ -10,7 +15,9 @@ import ttc2019.metamodels.bdd.Leaf;
 import ttc2019.metamodels.bdd.Subtree;
 import ttc2019.metamodels.bdd.Tree;
 import ttc2019.metamodels.tt.Cell;
+import ttc2019.metamodels.tt.Port;
 import ttc2019.metamodels.tt.Row;
+import ttc2019.metamodels.tt.TTFactory;
 import ttc2019.metamodels.tt.TruthTable;
 
 /**
@@ -31,31 +38,118 @@ public class TreeValidator {
 
 		int iRow = 0;
 		for (Row ttRow : tt.getRows()) {
-			if (!validate(iRow, ttRow, bdd.getTree())) {
-				return false;
+			for (Row completedRow : completeRows(tt, ttRow)) {
+				if (!validate(iRow, completedRow, bdd.getTree())) {
+					return false;
+				}
 			}
 			iRow++;
 		}
 		return true;
 	}
 
+	/**
+	 * Returns a collection of rows that define all the available input ports. Some
+	 * rows may only define some of their values. The output port assignments will
+	 * not be changed.
+	 */
+	private List<Row> completeRows(TruthTable tt, Row row) {
+		Set<ttc2019.metamodels.tt.InputPort> ttInputs = getTruthTableInputs(tt);
+		Set<ttc2019.metamodels.tt.InputPort> rowInputs = getRowInputs(row);
+
+		// If there are no unspecified inputs, the row is complete and can be returned as-is
+		ttInputs.removeAll(rowInputs);
+		if (ttInputs.isEmpty()) {
+			return Collections.singletonList(row);
+		}
+
+		// There are unspecified inputs: use recursion to build up copies of the row
+		// that add the missing values.
+		List<Row> completedRows = new ArrayList<>();
+		completeRows(tt, row, ttInputs, completedRows);
+		return completedRows;
+	}
+
+	private void completeRows(TruthTable tt, Row row, Set<ttc2019.metamodels.tt.InputPort> remainingInputs, List<Row> completed) {
+		if (remainingInputs.isEmpty()) {
+			completed.add(row);
+		} else {
+			final Set<ttc2019.metamodels.tt.InputPort> newRemaining = new HashSet<>(remainingInputs);
+			final Iterator<ttc2019.metamodels.tt.InputPort> itNewRemaining = newRemaining.iterator();
+			final ttc2019.metamodels.tt.InputPort port = itNewRemaining.next();
+			itNewRemaining.remove();
+			completeRows(tt, row, completed, newRemaining, port, true);
+			completeRows(tt, row, completed, newRemaining, port, false);
+		}
+	}
+
+	private void completeRows(TruthTable tt, Row row, List<Row> completed, final Set<ttc2019.metamodels.tt.InputPort> newRemaining, final ttc2019.metamodels.tt.InputPort port, boolean value) {
+		Row copyRow = TTFactory.eINSTANCE.createRow();
+		for (Cell cOriginal : row.getCells()) {
+			Cell cCopy = TTFactory.eINSTANCE.createCell();
+			cCopy.setPort(cOriginal.getPort());
+			cCopy.setValue(cOriginal.isValue());
+			copyRow.getCells().add(cCopy);
+		}
+
+		Cell cNew = TTFactory.eINSTANCE.createCell();
+		cNew.setPort(port);
+		cNew.setValue(value);
+		copyRow.getCells().add(cNew);
+
+		completeRows(tt, copyRow, newRemaining, completed);
+	}
+
+	private Set<ttc2019.metamodels.tt.InputPort> getRowInputs(Row row) {
+		Set<ttc2019.metamodels.tt.InputPort> rowInputs = new HashSet<>();
+		for (Cell c : row.getCells()) {
+			if (c.getPort() instanceof ttc2019.metamodels.tt.InputPort) {
+				rowInputs.add((ttc2019.metamodels.tt.InputPort) c.getPort());
+			}
+		}
+		return rowInputs;
+	}
+
+	private Set<ttc2019.metamodels.tt.InputPort> getTruthTableInputs(TruthTable tt) {
+		Set<ttc2019.metamodels.tt.InputPort> ttInputs = new HashSet<>();
+		for (Port p : tt.getPorts()) {
+			if (p instanceof ttc2019.metamodels.tt.InputPort) {
+				ttInputs.add((ttc2019.metamodels.tt.InputPort) p);
+			}
+		}
+		return ttInputs;
+	}
+
 	private boolean validate(int iRow, Row ttRow, Tree tree) {
 		final List<Assignment> assignments = findAssignments(ttRow, tree); 
 		if (assignments.isEmpty()) {
-			System.err.println(String.format("Row %d of TT did not produce any assignments in BDD", iRow));
+			System.err.println(String.format("Row %d of TT did not produce any assignments in BDD", ttRow, iRow));
 		}
 
 		for (Assignment a : assignments) {
 			final String oPortName = a.getPort().getName();
 			final boolean expectedResult = getExpectedResult(ttRow, oPortName);
 			if (expectedResult != a.isValue()) {
-				System.err.println(String.format("Row %d of TT produces unexpected result %s in BDD", iRow, a.isValue()));
+				System.err.println(String.format("Row %d of TT (%s) produces unexpected result O_%s = %s in BDD", iRow, formatRow(ttRow), oPortName, a.isValue()));
 				return false;
 			}
 		}
 
 		// No mismatches found!
 		return true;
+	}
+
+	private Object formatRow(Row ttRow) {
+		final List<String> inputs = new ArrayList<>(ttRow.getCells().size());
+		for (Cell c : ttRow.getCells()) {
+			inputs.add(
+				((c.getPort() instanceof ttc2019.metamodels.tt.InputPort) ? "I_" : "O_")
+				+ c.getPort().getName()
+				+ " = "
+				+ c.isValue()
+			);
+		}
+		return String.join(", ", inputs);
 	}
 
 	private boolean getExpectedResult(Row ttRow, String oPortName) {
@@ -81,16 +175,10 @@ public class TreeValidator {
 				}
 			}
 
-			/*
-			 * (2019-05-23 Artur Boronat) If the row in the table does not require a
-			 * specific value for the port checked in this subtree, try both values.
-			 */
-			List<Assignment> zeroList = findAssignments(ttRow, sb.getTreeForZero());
-			if (zeroList.isEmpty()) {
-				return findAssignments(ttRow, sb.getTreeForOne());
-			} else {
-				return zeroList;
-			}
+			// NOTE: since each row with unspecified input variables is completed into
+			// multiple fully specified rows, the original fix from Artur Boronat is no
+			// longer required.
+			throw new NoSuchElementException("Could not find a matching TT cell for the BDD port " + sbInputPort.getName());
 		} else {
 			throw new IllegalArgumentException("Tree must be a subtree or a leaf");
 		}
